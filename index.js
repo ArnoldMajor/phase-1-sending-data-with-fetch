78934d84cd3680873295da1f490930648e056aa7
Arnold Mwangakala <arnoldmwangakala@gmail.com>
2024-12-12 20:23:43 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            const id = document.createElement('p');
+            document.querySelector('body').appendChild(id);
+            id.textContent = data.id
+        })
+        .catch((error) => {
+            const errorMessage = document.createElement('p');
+            document.querySelector('body').appendChild(errorMessage);
+            errorMessage.textContent = error.message;
+        })
+}
